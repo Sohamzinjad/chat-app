58f8c7362f6e700b81c77ffeef80bcb139cd8ec2
soham zinjad <sohamzinjad@sohams-MacBook-Air.local>
2026-01-06 00:20:00 +0530

    Fix Express wildcard route and Render config

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,7 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
@@ -32,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
